28d8e164b59486b22eaac79182d75f62902ecd7f
Zac Melton <zac.melton@outlook.com>
2020-01-23 15:54:52 -0600

    Added tests for Add method and built the add method. Added Count and Capacity properites. Made constructor for the custom List.

--- CustomListBuild/CustomList.cs
+++ CustomListBuild/CustomList.cs
@@ -9,13 +9,47 @@ namespace CustomListBuild
     public class CustomList<T>
     {
         T[] items;
-        public int count;
+        private int count;
+        private int capacity;
 
-        public void Add(T item)
+        public int Count { get => count; }
+
+        public int Capacity { get => capacity; set => capacity = value; }
+
+        //indexer
+        public T this[int index]
         {
+            get => items[index];
 
+            set => items[index] = value;
         }
 
+        //constructor
+        public CustomList()
+        {
+            capacity = 4;
+            items = new T[capacity];
+            count = 0;
+        }
+
+        public void Add(T value)
+        {
+            if(count == capacity)
+            {
+                capacity *= 2;
+                T[] temporaryItems = items;
+                items = new T[capacity];
+                int index = 0;
+
+                foreach (T item in temporaryItems)
+                {
+                    items[index] = item;
+                    index++;
+                }
+            }
+            items[count] = value;
+            count++;
+        }
     }
 
 
--- CustomListBuildTest/CustomListTest.cs
+++ CustomListBuildTest/CustomListTest.cs
@@ -12,12 +12,310 @@ namespace CustomListBuildTest
         {
             //Arrange
             CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int expected = 1;
 
             //Act
+            items.Add(value);
+            int actual = items[0];
 
             //Assert
+            Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void CheckAddForAddingTwoItems()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int expected = 3;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            int actual = items[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFifthItemTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int thirdValue = 5;
+            int fourthValue = 7;
+            int fifthValue = 9;
+            int expected = 9;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            items.Add(thirdValue);
+            items.Add(fourthValue);
+            items.Add(fifthValue);
+            int actual = items[4];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void CheckOrderAfterAddFifthElementTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int thirdValue = 5;
+            int fourthValue = 7;
+            int fifthValue = 9;
+            int expected = 3;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            items.Add(thirdValue);
+            items.Add(fourthValue);
+            items.Add(fifthValue);
+            int actual = items[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckCountWhenEmptyTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int expected = 0;
+
+            //Act
+            int actual = items.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckCountAfterAddOneItemTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 5;
+            int expected = 1;
+
+            //Act
+            items.Add(value);
+            int actual = items.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void CheckCountAfterAddThreeItemsTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 5;
+            int secondValue = 8;
+            int thirdValue = 12;
+            int expected = 3;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            items.Add(thirdValue);
+            int actual = items.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckCountAfterAddFiveItemsTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int thirdValue = 5;
+            int fourthValue = 7;
+            int fifthValue = 9;
+            int expected = 5;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            items.Add(thirdValue);
+            items.Add(fourthValue);
+            items.Add(fifthValue);
+            int actual = items.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DefaultCapacityTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int expected = 4;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            int actual = items.Capacity;
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AfterAddFifthItemCapacityTest()
+        {
+            //Arrange
+            CustomList<int> items = new CustomList<int>();
+            int value = 1;
+            int secondValue = 3;
+            int thirdValue = 5;
+            int fourthValue = 7;
+            int fifthValue = 9;
+            int expected = 8;
+
+            //Act
+            items.Add(value);
+            items.Add(secondValue);
+            items.Add(thirdValue);
+            items.Add(fourthValue);
+            items.Add(fifthValue);
+            int actual = items.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestMethod]
+        //public void RemoveTest()
+        //{
+        //    //Arrange
+        //    CustomList<int> items = new CustomList<int>();
+        //    int value = 1;
+        //    int secondValue = 3;
+        //    int thirdValue = 5;
+
+        //    //Act
+        //    items.Add(value);
+        //    items.Add(secondValue);
+        //    items.Add(thirdValue);
+        //    items.Remove(secondValue);
+
+        //    //Assert
+        //    Assert.AreEqual(thirdValue, items[1]);
+        //}
+
+        //[TestMethod]
+        //public void CheckCountAfterRemoveTest()
+        //{
+        //    //Arrange
+        //    CustomList<int> items = new CustomList<int>();
+        //    int value = 5;
+        //    int secondValue = 7;
+        //    int expectedCount = 1;
+
+        //    //Act
+        //    items.Add(value);
+        //    items.Add(secondValue);
+
+        //    //Assert
+        //    Assert.AreEqual(expectedCount, items.Count);
+        //}
+
+        //[TestMethod]
+        //public void ToStringTest()
+        //{
+        //    //Arrange
+        //    CustomList<int> items = new CustomList<int>();
+        //    int value = 1;
+        //    int secondValue = 3;
+        //    int thirdValue = 5;
+        //    string expected = "135";
+
+        //    //Act
+        //    items.Add(value);
+        //    items.Add(secondValue);
+        //    items.Add(thirdValue);
+        //    string actual = items.ToString();
+
+        //    //Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+        //[TestMethod]
+        //public void CheckAddListTogether()
+        //{
+        //    //Arrange
+        //    CustomList<int> items = new CustomList<int>();
+        //    CustomList<int> itemsTwo = new CustomList<int>();
+        //    CustomList<int> added = new CustomList<int>();
+        //    string expectedResult = "135246";
+
+        //    //Act
+        //    items.Add(1);
+        //    items.Add(3);
+        //    items.Add(5);
+        //    itemsTwo.Add(2);
+        //    itemsTwo.Add(4);
+        //    itemsTwo.Add(6);
+        //    added.Add(items[0]);
+        //    added.Add(items[1]);
+        //    added.Add(items[2]);
+        //    added.Add(itemsTwo[0]);
+        //    added.Add(itemsTwo[1]);
+        //    added.Add(itemsTwo[2]);
+
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, added.ToString());
+        //}
+
+        //[TestMethod]
+        //public void ZipTest()
+        //{
+        //    //Arrange
+        //    CustomList<int> odd = new CustomList<int>();
+        //    CustomList<int> even = new CustomList<int>();
+        //    CustomList<int> zipped = new CustomList<int>();
+        //    string expectedResult = "123456";
+
+        //    //Act
+        //    odd.Add(1);
+        //    odd.Add(3);
+        //    odd.Add(5);
+        //    even.Add(2);
+        //    even.Add(4);
+        //    even.Add(6);
+        //    zipped.Add(odd[0]);
+        //    zipped.Add(even[0]);
+        //    zipped.Add(odd[1]);
+        //    zipped.Add(even[1]);
+        //    zipped.Add(odd[2]);
+        //    zipped.Add(even[2]);
+
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, zipped.ToString());
+        //}
     }
 }
